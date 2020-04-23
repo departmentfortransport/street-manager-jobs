8bd389f54d3e63994fff15cdd2c25ade9efabee3
Peadar Kelly <peadark@kainos.com>
2020-04-23 15:28:20 +0100

    Added missing ioc binding

--- src/job-1/ioc.ts
+++ src/job-1/ioc.ts
@@ -7,6 +7,7 @@ import TYPES from './types'
 import { JOB_1_INT_FIELD, JOB_1_STR_FIELD } from './config'
 import Job1Job from './job'
 import Job1Dao from './daos/job1Dao'
+import Job1Service from './services/job1Service'
 import Logger from '../common/utils/logger'
 
 const iocContainer = new Container()
@@ -28,6 +29,7 @@ iocContainer.bind<string>(TYPES.JOB_1_STR_FIELD).toConstantValue(JOB_1_STR_FIELD
 iocContainer.bind<Job1Dao>(TYPES.Job1Dao).to(Job1Dao).inSingletonScope()
 
 // Services
+iocContainer.bind<Job1Service>(TYPES.Job1Service).to(Job1Service)
 iocContainer.bind<Logger>(TYPES.Logger).to(Logger)
 
 export default iocContainer
